e1e1a5b8b3c3aeb6209e78a308b9fed57170a9ad
Henrique Costa <henriquefcosta19@gmail.com>
2023-07-20 20:06:57 -0300

    changes for docker improvement

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 import express from "express";
 import urls from "./urlsRoutes.js";
-//import user from "./userRoutes.js";
 import auth from "./authRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).json({ message: "Encurtador de URLS...em progresso." });
   });
-  app.use(express.json(), urls, /*user,*/ auth);
+  app.use(express.json(), urls, auth);
 };
 
 export default routes;
