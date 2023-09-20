0658314ddf3a3493b4b6b22c4a2b098e3b180884
Henrique Costa <henriquefcosta19@gmail.com>
2023-09-20 20:21:28 -0300

    app.js host

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ dotEnv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const host = "0.0.0.0";
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 routes(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, host, () => {
   console.log(`SERVIDOR OUVINDO NA PORTA: ${PORT}`);
 });
 
